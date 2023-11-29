881ea87f6c7b3f9badcb1e380a790aa8f5ac701e
marky376 <tech_star@Mark1-thinkpad>
2023-11-29 05:57:01 +0300

    A commit on my first  python Alx projects

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import sys
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+
+string = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+sys.stderr.write(string + "\n")
 sys.exit(1)
