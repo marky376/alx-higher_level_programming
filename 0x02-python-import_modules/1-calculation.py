cb74932414dea116acec40ab58b4339b9f166925
marky376 <tech_star@Mark1-thinkpad>
2023-11-30 16:35:17 +0300

    A commint on python project Importing Modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,17 @@
+#!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 
 a = 10
 b = 5
 
-
-print("{} + {} = {}".format(a, b, add(a, b)))
-print("{} - {} = {}".format(a, b, sub(a, b)))
-print("{} / {} = {}".format(a, b, div(a, b)))
-print("{} * {} = {}".format(a, b, mul(a, b)))
+result_add = add(a, b)
+print("{} + {} = {}".format(a, b, result_add))
+result_sub = sub(a, b)
+print("{} - {} = {}".format(a, b, result_sub))
+result_mul = mul(a, b)
+print("{} * {} = {}".format(a, b, result_mul))
+result_div = div(a, b)
+print("{} / {} = {}".format(a, b, result_div))
 
 if __name__ == "__main__":
     pass
