a0c67f74319e21b5f676f168601acb9e973e0ac1
marky376 <mmutuamark@gmail.com>
2023-12-20 15:22:41 +0300

    A commit on a function that prints x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x = 0):
+    counter = 0
+
+    for i  in range(x):
+        try:
+            print(my_list[i], end="")
+            counter += 1
+        except IndexError:
+            break
+
+    print()
+    return (counter)
