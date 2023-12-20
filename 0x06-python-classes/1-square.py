5b528c91da7cdbefd368aab1de3c056b2360a21d
marky376 <mmutuamark@gmail.com>
2023-12-20 23:58:20 +0300

    A commit on a class (Square) that defines a square by:[based on 0-square.py]

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Square module that has size
+
+"""
+
+class Square:
+    """
+    A class Square that has size as an instance attribute
+
+    """
+    def __init__(self, size):
+        """
+        Instantiation with size (no type/value verification)
+        """
+        self.__size = size
+        """
+        Private instance attribute: size
+        """
