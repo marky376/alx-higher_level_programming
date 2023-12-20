c6c782ba3ed9cc55b50f4f31ee453855d3a9282d
marky376 <mmutuamark@gmail.com>
2023-12-20 16:05:23 +0300

    A commit on a function that divides element by element 2 lists

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    result_list = []
+    for i in range(0, list_length):
+        try:
+            result = my_list_1[1] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except TypeError:
+            print("wrong type")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+        finally:
+            result_list.append(result)
+    return result_list       
