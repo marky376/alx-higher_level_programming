8e345e396fe7f5a10e085fceef1a433d8695f195
marky376 <mmutuamark@gmail.com>
2024-01-05 00:55:53 +0300

    A commit on an empty class Rectangle that defines a rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-""" 
+"""
 Defines a Rectangle class
 """
 
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+Defines a Rectangle class
+"""
+
+class Rectangle:
+    """Represents a rectangle"""
+    def __init__(self, width=0,height=0):
+        """Initializing a new rectangle
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get the width of the rectangle"""
+        return self.__width
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    @property
