6603359812041eb77a8fe49c5ee9cb9362281ebb
marky376 <mmutuamark@gmail.com>
2024-01-06 05:43:20 +0300

    A commit on a function that prevents instantiating new LockedClass attributes

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Defines a locked class
+"""
+
+class LockedClass:
+    """
+    Prevents the user from dynamically creating new instance
+    attributes, except if the new attribute is called first_name
+    """
+    __slots__ = ["first_name"]
