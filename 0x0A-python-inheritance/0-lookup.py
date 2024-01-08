a6346e08d4ffd27f5d8ef5edbcd0e1d7e1b4ee0d
marky376 <mmutuamark@gmail.com>
2024-01-08 06:41:12 -0800

    A commit on a function that returns the list of available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module to use dir() function"""
+
+
+def lookup(obj):
+    """function that returns the list of available
+        attributes and methods of an object"""
+    return dir(obj)
