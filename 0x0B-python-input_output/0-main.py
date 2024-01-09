13216c3e4414b0d80bce0f2538665e4314005f3a
marky376 <mmutuamark@gmail.com>
2024-01-09 11:26:25 -0800

    A commit on files to work on

---
+++ 0x0B-python-input_output/0-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+read_file = __import__('0-read_file').read_file
+
+read_file("my_file_0.txt")
+
+guillaume@ubuntu:~/0x0B$ cat my_file_0.txt
+We offer a truly innovative approach to education:
+focus on building reliable applications and scalable systems, take on real-world challenges, collaborate with your peers. 
+
+A school every software engineer would have dreamt of!
