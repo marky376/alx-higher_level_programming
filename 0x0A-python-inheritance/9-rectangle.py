7ff817fb809084bf63f9a6726cfac38791cb8e46
marky376 <mmutuamark@gmail.com>
2024-01-08 20:01:03 -0800

    A commit on a class Rectangle that inherits from BaseGeometry 7-base_geometry (file)

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Class that defines a rectangle and inherits from BaseGeometry."""
+
+    def __init__(self, width, height):
+        """
+        Initializes a Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Returns the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns a string representation of the rectangle."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
