444ccde8c057cf0e4873a4d8089b549a4b2122be
marky376 <mmutuamark@gmail.com>
2024-02-15 09:51:56 +0300

    A commit on files to work on

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+SELECT tv_shows.title
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN tv_genres ON tv_show_genres.tv_genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
+ 
