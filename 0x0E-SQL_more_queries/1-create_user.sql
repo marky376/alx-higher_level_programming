0489ddc17adcbbd9958245e16df474d4f7ee117d
marky376 <mmutuamark@gmail.com>
2024-02-15 09:09:26 +0300

    A commit on files to work on

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- creates the MySQL server user user_0d_1.
+-- If the user user_0d_1 already exists, your script should not fail
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
