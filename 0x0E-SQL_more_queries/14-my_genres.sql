b5989022255adcf8adb9592afbb838fbf5ec0dab
marky376 <mmutuamark@gmail.com>
2024-02-15 09:50:44 +0300

    A commit on files to work on

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,6 @@
+SELECT tv_genres.name
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN tv_genres ON tv_show_genres.tv_genre_id = tv_genres.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
