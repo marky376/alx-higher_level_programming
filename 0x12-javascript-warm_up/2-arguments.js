b70cebc7d4b24d8cf2789128e015bbb3f95bc835
marky376 <mmutuamark@gmail.com>
2024-03-11 12:46:00 +0300

    A commit on some files to work on

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
 const argsCount = process.argv.length - 2;
 
-if (argsCount === 0)
-{
-	console.log('No arguments');
-}
-else if (argsCount === 1)
-{
-	console.log('Arguments found');
-}
-else
-{
-	console.log('Arguments found');
+if (argsCount === 0) {
+  console.log('No arguments');
+} else if (argsCount === 1) {
+  console.log('Arguments found');
+} else {
+  console.log('Arguments found');
 }
