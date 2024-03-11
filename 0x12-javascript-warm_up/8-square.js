1cbe3b64228ab340777c9808aa2c21689bf0df85
marky376 <mmutuamark@gmail.com>
2024-03-11 18:27:13 +0300

    A commit on some files to work on

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,3 +6,14 @@ if (isNaN(size))
 	console.log('Missing size');
 }
 else
+{
+	for (let i = 0; i < size; i++)
+	{
+		let row = ' ';
+		for (let j = 0; j < size; j++)
+		{
+			row+= 'X';
+		}
+		console.log(row);
+	}
+}
