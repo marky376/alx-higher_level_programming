69f4af01aabb9904fdac4a5552eb2b2708831827
marky376 <mmutuamark@gmail.com>
2024-03-11 19:13:55 +0300

    A commit on some files to work on

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,2 +1,15 @@
 #!/usr/bin/node
 
+function secondBiggest(args) {
+    const nums = args.map(Number).sort((a, b) => b - a);
+    return nums[1] || 0;
+}
+
+const args = process.argv.slice(2);
+
+if (args.length < 2) {
+    console.log(0);
+} else {
+    console.log(secondBiggest(args));
+}
+
