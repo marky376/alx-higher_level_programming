7127ef1a98091f3d19b5492af771ece958727f1a
marky376 <mmutuamark@gmail.com>
2024-03-11 19:09:50 +0300

    A commit on some files to work on

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,2 +1,19 @@
 #!/usr/bin/node
 
+function factorial(n)
+{
+	if (isNaN(n))
+	{
+		return 1;
+	}
+	if (n <= 0)
+	{
+		return 1;
+	}
+	return n * factorial(n - 1);
+
+}
+
+const num = parseInt(process.argv[2]);
+
+console.log(factorial(num));
