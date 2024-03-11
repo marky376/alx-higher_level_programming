df547ac9ae45abffde9b71db0af6f8d8fa09905b
marky376 <mmutuamark@gmail.com>
2024-03-11 18:13:30 +0300

    A commit on some files to work on

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,2 +1,14 @@
 #!/usr/bin/node
+const num = parseInt(process.argv[2]);
 
+if (isNaN(num) || num < 0)
+{
+	console.log('Missing number of occurences');
+}
+else
+{
+	for (let i = 0; i < num; i++)
+	{
+		console.log('C is fun');
+	}
+}
