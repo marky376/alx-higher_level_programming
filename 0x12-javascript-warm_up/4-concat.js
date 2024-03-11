391b9dcfed1c3f2515adba14ec4adda4d3538dde
marky376 <mmutuamark@gmail.com>
2024-03-11 17:06:15 +0300

    A commit on some files to work on

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
+const arg1 = process.argv[2] || 'undefined' ;
+const arg2 = process.argv[3] || 'undefined' ;
 
-
+console.log('${arg1} is ${arg2}');
