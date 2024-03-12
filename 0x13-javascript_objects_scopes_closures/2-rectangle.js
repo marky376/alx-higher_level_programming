8134517bccacc987d61df54e87d06de3f22c7f67
marky376 <mmutuamark@gmail.com>
2024-03-12 16:44:34 +0300

    A commit on some files to work on

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
-class Rectangle
-{
-	constructor(w, h)
-	{
-		if (w <= 0 || h <= 0)
-		{
-			return{};
-		}
-		this.width = w;
-		this.height = h;
-	}
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return {};
+    }
+    this.width = w;
+    this.height = h;
+  }
 }
 
 module.exports = Rectangle;
