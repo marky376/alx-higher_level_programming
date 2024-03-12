5c2fe04625429d9f748892f6473f80bdcd7f918b
marky376 <mmutuamark@gmail.com>
2024-03-12 16:52:26 +0300

    A commit on some files to work on

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,24 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0) {
+      // Create an empty object if w or h is not positive
+      return {};
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += 'X';
+      }
+      console.log(line);
+    }
+  }
+}
+
+module.exports = Rectangle;
 
