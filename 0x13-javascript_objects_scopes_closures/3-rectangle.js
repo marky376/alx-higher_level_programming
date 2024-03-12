856af3bd8c991fa83506d8825e48eb78e6eab2c1
marky376 <mmutuamark@gmail.com>
2024-03-12 16:50:40 +0300

    A commit on some files to work on

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,15 +8,17 @@ class Rectangle {
     this.height = h;
   }
 
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let line = ' ';
-	    for (let j = 0; j < this.width; i++)
-	    {
-		    line += 'X';
-	    }
+  print () 
+{	
+    	for (let i = 0; i < this.height; i++) 
+	{
+      		let line = ' ';
+		for (let j = 0; j < this.width; j++) 
+		{
+			line += 'X';
+		}
 	    console.log(line);
-    }
+	}
   }
 }
 
