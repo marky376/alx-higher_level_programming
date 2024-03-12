ba9022f8db40eba85df792b70af9556cf0050ee6
marky376 <mmutuamark@gmail.com>
2024-03-12 04:53:36 +0300

    A commit on some files to work on

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,1 +1,20 @@
+// 103-object_fct.js
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12,
+  incr: function() {
+    this.value++;
+  }
+};
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
 
