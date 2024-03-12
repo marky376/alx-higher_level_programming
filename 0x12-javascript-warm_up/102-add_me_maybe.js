1ee3002e4e8e4f3968c2089c875d37b89ecac8cc
marky376 <mmutuamark@gmail.com>
2024-03-12 04:48:04 +0300

    A commit on some files to work on

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -5,3 +5,8 @@ function addMeMaybe(number, theFunction) {
 
 module.exports.addMeMaybe = addMeMaybe;
 
+const addMeMaybe = require('./102-add_me_maybe').addMeMaybe;
+
+addMeMaybe(4, function (nb) {
+    console.log('New value: ' + nb);
+});
