2f5808bf270e978df1143589c9299b619f3dbd6e
marky376 <mmutuamark@gmail.com>
2024-03-13 13:31:44 +0300

    A commit on some files to work on

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
 
+exports.esrever = function (list) {
+  let reversedList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversedList.push(list[i]);
+  }
+  return reversedList;
+};
+
