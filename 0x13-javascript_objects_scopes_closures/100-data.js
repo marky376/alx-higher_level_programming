b44f84137c53ae86b3c7adb676aa2960cd4d7dee
marky376 <mmutuamark@gmail.com>
2024-03-13 13:42:44 +0300

    A commit on some files to work on

--- 0x13-javascript_objects_scopes_closures/100-data.js
+++ 0x13-javascript_objects_scopes_closures/100-data.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
-exports.list = [1, 2, 3, 4, 5];
+#!/usr/bin/node
+const { list } = require('./100-data');
+
+const newList = list.map((value, index) => value * index);
+
+console.log(list);
+console.log(newList);
+
