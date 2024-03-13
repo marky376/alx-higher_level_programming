831c3f056708e1bc8119a52bce39d716327674c5
marky376 <mmutuamark@gmail.com>
2024-03-13 13:15:55 +0300

    A commit on some files to work on

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,29 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0) {
+      // Create an empty object if w or h is not positive
+      return {};
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate() {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
+
