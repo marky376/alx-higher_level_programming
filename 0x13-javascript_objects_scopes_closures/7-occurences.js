49e3b0c7a1da4fca658df5aede62c20f6c93356b
marky376 <mmutuamark@gmail.com>
2024-03-13 13:25:52 +0300

    A commit on some files to work on

--- 0x13-javascript_objects_scopes_closures/7-occurences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurences.js
@@ -2,11 +2,10 @@
 
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  for (let element of list) {
+  for (const element of list) {
     if (element === searchElement) {
       count++;
     }
   }
   return count;
 };
-
