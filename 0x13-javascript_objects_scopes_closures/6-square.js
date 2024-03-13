bc859dd7d671b2d7edaa9384fb15b665b9f43fc9
marky376 <mmutuamark@gmail.com>
2024-03-13 13:22:10 +0300

    A commit on some files to work on

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,2 +1,17 @@
 #!/usr/bin/node
 
+const Square = require('./5-square');
+
+class SquareWithCharPrint extends Square {
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = SquareWithCharPrint;
+
