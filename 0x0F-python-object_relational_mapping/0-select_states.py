a3eb5492831c22876c01eba2ea832a73070766ae
marky376 <mmutuamark@gmail.com>
2024-03-14 20:25:01 +0300

    A commit on some files to use

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Execute SQL query
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all the rows
+    states = cursor.fetchall()
+
+    # Display results
+    for state in states:
+        print(state)
+
+    # Close cursor and connection
+    cursor.close()
+    db.close()
+
